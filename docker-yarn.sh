f8e398ce28eaf5b1f53ffbe7d1513b5f3f4347a6
V Ng <vee.vidify@gmail.com>
2021-03-10 20:37:36 +1100

    fix: wrapper script

--- docker-yarn.sh
+++ docker-yarn.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-module="$1"
-args="${@:2}"
-echo "yarn $module $args"
-docker run --rm -it -v "$(pwd):/app" -w "/app" -u $UID -e CHOKIDAR_USEPOLLING=true --env-file="$(pwd)/.env" node:12-alpine yarn $module $args
+args="$@"
+echo "yarn $args"
+docker run --rm -i -v "$(pwd):/app" -w "/app" -u $UID -e CHOKIDAR_USEPOLLING=true --env-file="$(pwd)/.env" node:12-alpine yarn $args
 
